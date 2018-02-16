ba43c46558adbb8c2b9abd094dda8a68765089a8
lol <lol@lol-X550LA>
2018-02-16 17:41:04 +0100

    Creación clase DeCarga

---
+++ src/alquilervehiculos/mvc/modelo/dominio/vehiculo/DeCarga.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package alquilervehiculos.mvc.modelo.dominio.vehiculo;
+
+/**
+ *
+ * @author lol
+ */
+public class DeCarga extends Vehiculo {
+    
+    public DeCarga(String matricula, String marca, String modelo, int cilindrada, int numeroPlazas, int pma) {
+        super(matricula, marca, modelo, new DatosTecnicosVehiculo(cilindrada, numeroPlazas, pma));
+    }
+
+    public DeCarga(Vehiculo vehiculo) {
+        super(vehiculo);
+    }
+    
+    public TipoVehiculo getTipoVehiculo(){
+        return TipoVehiculo.DE_CARGA;
+    }
+    
+    public double getPrecioEspecifico(){
+        return getDatosTecnicos().getPma() / FACTOR_PMA;
+    }
+}
